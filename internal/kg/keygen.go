e420ad7057476866649f9d54710195776b576718
Pablo_3_16 <ergle.pasha@gmail.com>
2025-07-13 01:38:17 +0300

    Implemented generation map part & added modpack field in quest struct

--- internal/kg/keygen.go
+++ internal/kg/keygen.go
@@ -5,15 +5,20 @@ import (
 	"strings"
 )
 
-type Lang string
-
-const (
-	Ru Lang = "ru_ru"
-	En Lang = "en_us"
-)
+// type Quest struct {
+// 		Number       int
+// 		ModpackName  string
+// 		Chapter      string
+// 		Id           string
+// 		Title        string
+// 		Subtitle     string
+// 		TaskTitles   map[int]string
+// 		Description  []string
+// 		OriginalText string
+// }
 
 // todo checking fields
-func (q Quest) GenerateKeys() string {
+func (q *Quest) GenerateKeys() string {
 	result := ""
 
 	isSavingDescription := false
@@ -30,23 +35,23 @@ func (q Quest) GenerateKeys() string {
 
 		// title
 		if strings.HasPrefix(line, "\t\t\ttitle: ") {
-			result += fmt.Sprintf("\t\t\ttitle: \"{homestead.%s.%s.quest%d.title}\"\n",
-				q.Chapter, q.Id, q.Number)
+			result += fmt.Sprintf("\t\t\ttitle: \"{%s.%s.%s.quest%d.title}\"\n",
+				q.ModpackName, q.Chapter, q.Id, q.Number)
 			continue
 		}
 
 		// subtitle
 		if strings.HasPrefix(line, "\t\t\tsubtitle: ") {
-			result += fmt.Sprintf("\t\t\tsubtitle: \"{homestead.%s.%s.quest%d.subtitle}\"\n",
-				q.Chapter, q.Id, q.Number)
+			result += fmt.Sprintf("\t\t\tsubtitle: \"{%s.%s.%s.quest%d.subtitle}\"\n",
+				q.ModpackName, q.Chapter, q.Id, q.Number)
 			continue
 		}
 
 		// description
 		if strings.HasPrefix(line, "\t\t\tdescription: ") {
 			if len(q.Description) == 1 {
-				result += fmt.Sprintf("\t\t\tdescription: [\"{homestead.%s.%s.quest%d.description0}\"]\n",
-					q.Chapter, q.Id, q.Number)
+				result += fmt.Sprintf("\t\t\tdescription: [\"{%s.%s.%s.quest%d.description0}\"]\n",
+					q.ModpackName, q.Chapter, q.Id, q.Number)
 				continue
 			}
 			result += "\t\t\tdescription: [\n"
@@ -58,8 +63,8 @@ func (q Quest) GenerateKeys() string {
 			if q.Description[numArrayDescription] == "" {
 				result += "\t\t\t\t\"\"\n"
 			} else {
-				result += fmt.Sprintf("\t\t\t\t\"{homestead.%s.%s.quest%d.description%d}\"\n",
-					q.Chapter, q.Id, q.Number, numDesctription)
+				result += fmt.Sprintf("\t\t\t\t\"{%s.%s.%s.quest%d.description%d}\"\n",
+					q.ModpackName, q.Chapter, q.Id, q.Number, numDesctription)
 				numDesctription++
 			}
 			numArrayDescription++
@@ -71,8 +76,8 @@ func (q Quest) GenerateKeys() string {
 
 		// task titles
 		if strings.HasPrefix(line, "\t\t\t\t\ttitle: ") {
-			result += fmt.Sprintf("\t\t\t\t\ttitle: \"{homestead.%s.%s.quest%d.task%d.title}\"\n",
-				q.Chapter, q.Id, q.Number, keys[numKey])
+			result += fmt.Sprintf("\t\t\t\t\ttitle: \"{%s.%s.%s.quest%d.task%d.title}\"\n",
+				q.ModpackName, q.Chapter, q.Id, q.Number, keys[numKey])
 			numKey++
 			continue
 		}
@@ -82,7 +87,45 @@ func (q Quest) GenerateKeys() string {
 	return result
 }
 
-// todo
-func (q *Quest) GenerateMap(lang Lang) string {
-	return ""
+func (q *Quest) GenerateMapPart(l string) (string, error) {
+	lang := Lang_array[l]
+	if lang.String() == "unknown" {
+		return "", fmt.Errorf("unknown language %s; please, check correct codes in internal/kg/lang.go", l)
+	}
+
+	numDesctription := 0
+	result := ""
+
+	// title
+	if q.Title != "" {
+		result += fmt.Sprintf(", \"%s.%s.%s.quest%d.title\": \"%s\"",
+			q.ModpackName, q.Chapter, q.Id, q.Number, q.Title)
+	}
+
+	// subtitle
+	if q.Subtitle != "" {
+		result += fmt.Sprintf(", \"%s.%s.%s.quest%d.subtitle\": \"%s\"",
+			q.ModpackName, q.Chapter, q.Id, q.Number, q.Subtitle)
+	}
+
+	// description
+	for _, description := range q.Description {
+		if description != "" {
+			result += fmt.Sprintf(", \"%s.%s.%s.quest%d.description%d\": \"%s\"",
+				q.ModpackName, q.Chapter, q.Id, q.Number, numDesctription, description)
+			numDesctription++
+		}
+	}
+
+	// task titles
+	for i, titleTask := range q.TaskTitles {
+		result += fmt.Sprintf(", \"%s.%s.%s.quest%d.task%d.title\": \"%s\"",
+			q.ModpackName, q.Chapter, q.Id, q.Number, i, titleTask)
+	}
+
+	if q.Number == 0 {
+		result = strings.TrimPrefix(result, ", ")
+	}
+
+	return result, nil
 }
