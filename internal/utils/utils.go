5c1ab414a2bc13d57b408b797b28dd369faaee51
Pablo_3_16 <ergle.pasha@gmail.com>
2025-07-13 17:07:27 +0300

    implemented CreateAndWriteFile func

---
+++ internal/utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func CreateAndWriteFile(filePath string, content string) error {
+	newF, err := os.Create(filePath)
+	if err != nil {
+		if !errors.Is(err, os.ErrExist) {
+			return fmt.Errorf("creating %s file error: %w", filePath, err)
+		}
+	}
+	defer newF.Close()
+
+	if _, err = newF.WriteString(content); err != nil {
+		if !errors.Is(err, os.ErrExist) {
+			return fmt.Errorf("writing %s file error: %w", filePath, err)
+		}
+	}
+	return nil
+}
